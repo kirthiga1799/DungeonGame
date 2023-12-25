31d6e8efbd14404cc8aef2da5f091e2a44de12f1
kirthiga1799 <148054615+kirthiga1799@users.noreply.github.com>
2023-12-25 16:52:46 +0530

    Update Dungeon_Ques1.java

--- Dungeon_Ques1.java
+++ Dungeon_Ques1.java
@@ -22,48 +22,58 @@ public class Dungeon_Game {
 			
 			
 			
-			int[][] arr = new int[row][col];
+			
+		System.out.println("Postion of Monster : ");
+		int monsR = sc.nextInt();
+		int monsC = sc.nextInt();
 
-        		System.out.println("Enter the number of pits: ");
-        		int pits = sc.nextInt();
-        		List<int[]> pitList = new ArrayList<>();
+		System.out.println("Position of Gold : ");
+		int goldR = sc.nextInt();
+		int goldC = sc.nextInt();
 
-        		for (int i = 0; i < pits; i++) {
-            		System.out.println("Position of pit " + (i + 1) + " :");
-            		int x = sc.nextInt() - 1;
-            		int y = sc.nextInt() - 1;
-            		pitList.add(new int[]{x, y});
-        	}
+		int[][] arr = new int[row][col];
+		System.out.println("Enter the number of pits : ");
+		int pit = sc.nextInt();
+		List<int[]> pitList = new ArrayList<>();
 
-        	for (int i = advR - 1; i >= goldR - 1; i--) {
-           	for (int j = advC - 1; j < col; j++) {
-                	if (containsPit(pitList, i, j)) {
-                   	 arr[i][j] = 999;
-                    	  continue;
-                }
-                if (i == advR - 1 && j == 0)
-                    continue;
-                else if (j == 0)
-                    arr[i][j] = arr[i + 1][j] + 1;
-                else if (i == advR - 1)
-                    arr[i][j] = arr[i][j - 1] + 1;
-                else
-                    arr[i][j] = Math.min(arr[i + 1][j], arr[i][j - 1]) + 1;
-            }
-        }
+		for (int i = 0; i < pit; i++) {
+			System.out.println("Position of pit " + (i + 1) + " :");
+			int pitR = sc.nextInt();
+			int pitC = sc.nextInt();
+			pitList.add(new int[] { pitR, pitC });
+		}
 
-        if (arr[goldR - 1][goldC - 1] >= 999)
-            System.out.println("No possible solution");
-        else
-            System.out.println("Minimum number of steps: " + arr[goldR - 1][goldC - 1]);
-    }
+		for (int i = advR - 1; i >= goldR - 1; i--) {
+			for (int j = advC; j < col; j++) {
+				if (containsPit(pitList, i, j)) {
+					arr[i][j] = 999;
+					continue;
+				}
+				if (i == advR - 1 && j == 0)
+					continue;
+				else if (j == 0)
+					arr[i][j] = arr[i + 1][j] + 1;
+				else if (i == advR - 1)
+					arr[i][j] = arr[i][j - 1] + 1;
+				else
+					arr[i][j] = Math.min(arr[i + 1][j], arr[i][j - 1]) + 1;
+			}
+		}
 
-    private static boolean containsPit(List<int[]> pitList, int x, int y) {
-        for (int[] pit : pitList) {
-            if (pit[0] == x && pit[1] == y) {
-                return true;
-            }
-        }
-        return false;
+		int monsterMinSteps = Math.abs(monsR - goldR) + Math.abs(monsC - goldC);
+
+		if (monsterMinSteps < arr[goldR - 1][goldC - 1]) {
+			System.out.println("No possible solution ");
+		} else
+			System.out.println("Minimum number of steps : " + arr[goldR - 1][goldC - 1]);
+	}
+
+	private static boolean containsPit(List<int[]> pitList, int x, int y) {
+		for (int[] pit : pitList) {
+			if (pit[0] == x && pit[1] == y) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
